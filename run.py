a26a398ff0a694ea2d0113e20469cdc87800b9be
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-03-01 14:37:50 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from getmac import get_mac_address
 
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Get Mac Address")
 
 but=st.button("Get The Mac")
